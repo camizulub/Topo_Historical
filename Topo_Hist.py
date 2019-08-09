21b465c11a2548c7fc750c6fc0b6183cb4dbf1c5
Camilo_Zuluaga <camilo.zuluaga.trader@gmail.com>
2019-08-09 15:56:19 -0500

    Filter for current time, tqdm deleted, json saving

--- Topo_Hist.py
+++ Topo_Hist.py
@@ -10,23 +10,19 @@
 import pandas as pd
 from datetime import datetime, timedelta 
 from ib_insync import *
-from tqdm import tqdm
+import pytz
 
 class Topo:
 
     def __init__(self):
         '''Initializes the topo atribuites.'''
-        self.clientid = 8
-        #input('\tClient ID: ')
-        self.ticket = 'ES'
-        #input('\tTicket: ')
-        self.exch = 'GLOBEX'
-        #input('\tExchange: ')
+        self.clientid = input('\tClient ID: ')
+        self.ticket = input('\tTicket: ')
+        self.exch = input('\tExchange: ')
         self.contract = Contract(secType='CONTFUT', exchange=self.exch, symbol=self.ticket)
         self.start= '20190101 00:00:00'
         self.data_type = 'BID_ASK'
         self.n_days = (datetime.now() - datetime.strptime(self.start, '%Y%m%d %H:%M:%S')).days
-        print(self.n_days)
         self.counter = 0
         self.data = []
 
@@ -36,15 +32,13 @@ class Topo:
         ib.qualifyContracts(self.contract)
         
     def set_range(self):
-        ''' Sets the objective counter to perform the loop.
-        '''
+        ''' Sets the objective counter to perform the loop.'''
         self.counter_range = round(self.n_days*24)
         print('Counter Range= ' + str(self.counter_range))
 
     def looping(self):
-        ''' Creates the routine for downloading the historical data.
-        '''
-        for self.counter in tqdm(range(self.counter_range)):
+        ''' Creates the routine for downloading the historical data.'''
+        for self.counter in range(self.counter_range):
             if self.counter == 0: 
                 hist = ib.reqHistoricalTicks(
                         self.contract,
@@ -56,38 +50,37 @@ class Topo:
                 df = util.df(hist)
                 self.data.append(df)
             else:
-                hist = ib.reqHistoricalTicks(
-                        self.contract,
-                        startDateTime=df.iloc[-1,0].strftime("%Y%m%d %H:%M:%S"),
-                        endDateTime='',
-                        numberOfTicks=1000,
-                        whatToShow=self.data_type,
-                        useRth=False)
-                df = util.df(hist)
-                self.data.append(df)
-                '''if self.counter%5 == 0:
-                    time.sleep(2)
-                if self.counter%59 == 0:
-                    time.sleep(60*10)'''
+                if df.iloc[-1,0] >= (datetime.utcnow().replace(tzinfo=pytz.UTC) - timedelta(hours=10)):
+                    break
+                else:
+                    hist = ib.reqHistoricalTicks(
+                            self.contract,
+                            startDateTime=df.iloc[-1,0].strftime("%Y%m%d %H:%M:%S"),
+                            endDateTime='',
+                            numberOfTicks=1000,
+                            whatToShow=self.data_type,
+                            useRth=False)
+                    df = util.df(hist)
+                    self.data.append(df)
                 
     def save_data(self):
         ''' Saves the data in a CSV file, if the name is not changed it overwirtes the file
-        in the destination folder.
-        '''
-        #self.data.reverse()
+        in the destination folder.'''
         final = pd.concat(self.data)
         final = final.drop(columns= ['tickAttribBidAsk'] )
-        final.to_csv('C:/Users/MiloZB/Desktop/{}(ticks).csv'.format(self.ticket))
+        final.to_json(r'/home/camilo/Dropbox/Historical Data/{}(ticks).json'.format(self.ticket))
 
+    def digging(self):
+        self.connect()
+        self.set_range()
+        self.looping()
+        self.save_data()
 
 if __name__ == '__main__':
 
     ib = IB()
     print("Starting")
     topito = Topo()
-    topito.connect()
-    topito.set_range()
-    topito.looping()
-    topito.save_data()
+    topito.digging
     print("Finished")
         
