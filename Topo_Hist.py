ce5578fe465c37000d75317258a3ee2b0345f9fe
CAMILO_ZULUAGA <camilo.zuluaga.trader@gmail.com>
2019-11-25 08:26:33 -0500

    Seconds data

--- Topo_Hist.py
+++ Topo_Hist.py
@@ -10,7 +10,8 @@
 import pandas as pd
 from datetime import datetime, timedelta, timezone
 import pytz
-from ib_insync import *
+from ib_insync import IB, util, Contract
+from tqdm import tqdm
 
 class Topo:
 
@@ -21,60 +22,68 @@ class Topo:
         self.exch = input('\tExchange: ')
         self.contract = Contract(secType='CONTFUT', exchange=self.exch, symbol=self.ticket)
         self.format = "%Y%m%d %H:%M:%S"
-        self.start = '20190811 00:00:00'
+        self.start = '20191001 00:00:00'
         self.startdt = datetime.strptime(self.start, self.format)
         self.data_type = 'TRADES'
-        self.n_seconds = (datetime.now() - self.startdt).seconds
+        self.n_seconds = ((datetime.now() - self.startdt).days*60*60*24) + (datetime.now() - self.startdt).seconds
         self.counter = 0
         self.data = []
         self.tz = pytz.timezone('US/Eastern')
 
     def connect(self):
         '''Connects to IB Gateway or TWS.'''
-        ib.connect('127.0.0.1', 7497, clientId=self.clientid)
+        ib.connect('127.0.0.1', 7498, clientId=self.clientid)
         ib.qualifyContracts(self.contract)
         
     def set_range(self):
         ''' Sets the objective counter to perform the loop.'''
-        self.counter_range = round(self.n_seconds/24)
+        self.counter_range = round(self.n_seconds/2000)
         print('Counter Range= ' + str(self.counter_range))
 
     def looping(self):
         ''' Creates the routine for downloading the historical data.'''
-        for self.counter in range(self.counter_range):
+        for self.counter in tqdm(range(self.counter_range)):
             if self.counter == 0: 
-                hist = ib.reqHistoricalTicks(
-                        self.contract,
-                        startDateTime='',
-                        endDateTime=datetime.now(),
-                        numberOfTicks=1000,
+                hist = ib.reqHistoricalData(
+                        self.contract, endDateTime='',
+                        durationStr='2000 S',
+                        barSizeSetting='1 secs',
                         whatToShow=self.data_type,
-                        useRth=False)
+                        useRTH=False,
+                        formatDate=1)
                 df = util.df(hist)
                 self.data.append(df)
             else:
-                if df.iloc[0,0].replace(tzinfo=timezone.utc).astimezone(tz=self.tz) <= self.startdt.replace(tzinfo=timezone.utc).astimezone(tz=self.tz):
-                        break
-                else:
-                    hist = ib.reqHistoricalTicks(
+                if self.counter%5==0:
+                    ib.sleep(2)
+                if self.counter%60==0:
+                    ib.sleep(575)
+                try:
+                    hist = ib.reqHistoricalData(
                             self.contract,
-                            startDateTime='',
-                            endDateTime=df.iloc[0,0].replace(tzinfo=timezone.utc).astimezone(tz=self.tz).strftime(self.format),
-                            numberOfTicks=1000,
+                            endDateTime=df.iloc[0,0].strftime(self.format),
+                            durationStr='2000 S',
+                            barSizeSetting='1 secs',
                             whatToShow=self.data_type,
-                            useRth=False)
+                            useRTH=False,
+                            formatDate=1)
                     df = util.df(hist)
-                    print(df.iloc[0,0].replace(tzinfo=timezone.utc).astimezone(tz=self.tz))
                     self.data.append(df)
+                except:
+                    print('Exception Ocurred')
+                    break
                 
     def save_data(self):
         ''' Saves the data in a CSV file, if the name is not changed it overwirtes the file
         in the destination folder.'''
         self.data.reverse()
         final = pd.concat(self.data)
-        final.drop(columns= ['tickAttribLast', 'exchange', 'specialConditions'], inplace=True)
-        final.set_index('time', inplace=True)
-        final.to_csv('/home/camilo/Dropbox/Codigos/Data/{}(ticks){}.csv'.format(self.ticket, self.start))
+        final .columns = map(lambda x: x.capitalize(), final.columns)
+        time_format = '%Y-%m-%d %H:%M:%S.%f'
+        final['Date'] = pd.to_datetime(final['Date'], format= time_format)
+        final.set_index('Date', inplace=True)
+        final = final.drop(columns= ['Barcount', 'Average'] )
+        final.to_csv('C:/Users/MiloZB/Dropbox/Codigos/Data/{}(secIB).csv'.format(self.ticket))
 
     def digging(self):
         self.connect()
