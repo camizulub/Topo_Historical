0ab0756ee3d18c6c2b66ed589baf4d9cbec12127
Camilo Zuluaga <49444585+camizulub@users.noreply.github.com>
2022-04-17 16:23:11 -0500

    Update Data_Cleaner.py
    A wild card for BA data being erased

--- Data_Cleaner.py
+++ Data_Cleaner.py
@@ -7,7 +7,7 @@ symbols = ['GC', 'SI', 'PL', 'PA', 'MGC', 'QO', 'QI', 'MXP', 'ES', 'CL', 'NQ', '
 
 for symbol in symbols:
 # Get a list of all the file paths that ends with wildcard
-    fileList = glob.glob("Topo_Data/{}/{}_2*ticks.csv".format(symbol, symbol))
+    fileList = glob.glob("Topo_Data/{}/{}_2*ticks*.csv".format(symbol, symbol))
     print('Deleting the following file for {}'.format(symbol))
     print(fileList)
     # Iterate over the list of filepaths & remove each file.
